a5ddeb82a184aeeea1d591effe4e011937db04c6
Aakash260 <akashnirwan26@gmail.com>
2024-11-12 16:47:57 +0530

    till like

--- my-project/src/components/Post.jsx
+++ my-project/src/components/Post.jsx
@@ -6,10 +6,18 @@ import { Bookmark, MessageCircle, MoreHorizontal, Send } from 'lucide-react'
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import CommentDialog from "./CommentDialog";
 import { useState } from "react";
-
+import { useSelector,useDispatch } from "react-redux";
+import axios from "axios";
+import { toast } from "sonner";
+import { setPosts } from "@/redux/postSlice";
 const Post = ({post}) => {
   const [text,setText] =useState("")
   const [open,setOpen]=useState(false)
+  const {user}=useSelector(store=>store.auth)
+  const {posts} = useSelector(store=>store.post)
+  const dispatch = useDispatch();
+  const [liked, setLiked] = useState(post.likes.includes(user?._id) || false);
+  const [postLike, setPostLike] = useState(post.likes.length);
 const changeEventhandler=(e)=>{
 const inputText=e.target.value;
 if(inputText.trim()){
@@ -18,6 +26,45 @@ if(inputText.trim()){
   setText("");
 }
 }
+const deletePostHandler = async () => {
+  try {
+      const res = await axios.delete(`http://localhost:8000/api/v1/postRoute/delete/${post?._id}`, { withCredentials: true })
+      if (res.data.success) {
+        const updatedPostData = posts.filter((postItem) => postItem?._id !== post?._id);
+        dispatch(setPosts(updatedPostData));
+          toast.success(res.data.message);
+      }
+  } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.messsage);
+  }
+}
+
+const likeOrDislikeHandler = async () => {
+  try {
+      const action = liked ? 'dislike' : 'like';
+      const res = await axios.get(`http://localhost:8000/api/v1/postRoute/${post._id}/${action}`, { withCredentials: true });
+       
+      if (res.data.success) {
+          const updatedLikes = liked ? postLike - 1 : postLike + 1;
+          setPostLike(updatedLikes);
+          setLiked(!liked);
+
+          // apne post ko update krunga
+          const updatedPostData = posts.map(p =>
+              p._id === post._id ? {
+                  ...p,
+                  likes: liked ? p.likes.filter(id => id !== user._id) : [...p.likes, user._id]
+              } : p
+          );
+          dispatch(setPosts(updatedPostData));
+          toast.success(res.data.message);
+      }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
   return (
     <div className="my-8 w-full max-w-sm mx-auto">
       <div className="flex items-center justify-between">
@@ -42,9 +89,12 @@ if(inputText.trim()){
             <Button variant="ghost" className="cursor-pointer w-fit ">
               Add to favourite
             </Button>
-            <Button variant="ghost" className="cursor-pointer w-fit ">
+            {
+              user && user?._id===post?.author._id && <Button onClick={deletePostHandler} variant="ghost" className="cursor-pointer w-fit ">
               Delete
             </Button>
+            }
+            
           </DialogContent>
         </Dialog>
       </div>
@@ -57,7 +107,7 @@ if(inputText.trim()){
         <div className="flex items-center justify-between">
 
         <div className="flex items-center gap-3">
-        <FaRegHeart size={"22px"} className="cursor-pointer hover:text-gray-600"/>
+        <FaRegHeart onClick={likeOrDislikeHandler} size={"22px"} className="cursor-pointer hover:text-gray-600"/>
         <MessageCircle onClick={()=>setOpen(true)} className="cursor-pointer hover:text-gray-600" />
         <Send className="cursor-pointer hover:text-gray-600"/>
         <Bookmark className="cursor-pointer hover:text-gray-600" />
